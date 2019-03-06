c893ca6c12aee36b96190e71a9a1e93cc56d6207
Samwell <samwellwang@qq.com>
2019-03-06 21:09:46 +0800

    提交问价

---
+++ 土地管理地理信息系统/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 土地管理地理信息系统
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
